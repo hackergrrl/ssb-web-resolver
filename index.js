dd4f0b56e27e595504356ae1ecefd402fa1e1991
Stephen Whitmore <sww@eight45.net>
2018-04-16 14:12:13 -0700

    fix: WANT blobs that are missing

--- index.js
+++ index.js
@@ -14,36 +14,45 @@ function resolve (sbot, components, cb) {
   // get root blob
   sbot.blobs.size(id, function (err, size) {
     if (err) return cb(err)
-    if (size == null) return cb(null, self.askWantBlobsForm([id]))
-    pull(sbot.blobs.get(id), pull.collect(function (err, chunks) {
-      if (err) return cb(err)
-      var text = Buffer.concat(chunks).toString()
-      try {
-        // if this succeeds, it's a directory
-        var json = JSON.parse(text)
-        if (!json.links) throw new Error('show me as a raw file')
-        var link = components.shift()
+    if (size == null) {
+      sbot.blobs.want(id, function (err) {
+        if (err) return cb(err)
+        get()
+      })
+    }
+    else get()
 
-        // if components[1] is empty, return the directory as text
-        if (!link) {
-          // TODO: redirect to index.htm[l]
-          return cb(null, text)
-        } else if (!json.links[link]) {
-          // no such link exists
-          return cb(new Error('404'))
-        } else {
-          // recurse
-          resolve(sbot, [json.links[link]].concat(components), cb)
+    function get () {
+      pull(sbot.blobs.get(id), pull.collect(function (err, chunks) {
+        if (err) return cb(err)
+        var text = Buffer.concat(chunks).toString()
+        try {
+          // if this succeeds, it's a directory
+          var json = JSON.parse(text)
+          if (!json.links) throw new Error('show me as a raw file')
+          var link = components.shift()
+
+          // if components[1] is empty, return the directory as text
+          if (!link) {
+            // TODO: redirect to index.htm[l]
+            return cb(null, text)
+          } else if (!json.links[link]) {
+            // no such link exists
+            return cb(new Error('404'))
+          } else {
+            // recurse
+            resolve(sbot, [json.links[link]].concat(components), cb)
+          }
+        } catch (e) {
+          // if this happens, it's a file blob
+          // if components[1] exists, this is a 404; otherwise we arrived!
+          if (components[0]) {
+            return cb(new Error('404'))
+          } else {
+            cb(null, text)
+          }
         }
-      } catch (e) {
-        // if this happens, it's a file blob
-        // if components[1] exists, this is a 404; otherwise we arrived!
-        if (components[0]) {
-          return cb(new Error('404'))
-        } else {
-          cb(null, text)
-        }
-      }
-    }))
+      }))
+    }
   })
 }
