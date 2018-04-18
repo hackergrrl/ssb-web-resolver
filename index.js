6bacf4363109a5b7be1a6ae6c3b5829380db9b48
Stephen Whitmore <sww@eight45.net>
2018-04-17 19:53:55 -0700

    feat: resolve "web-root" messages

--- index.js
+++ index.js
@@ -5,58 +5,89 @@ module.exports = resolve
 function resolve (sbot, components, cb) {
   var self = this
 
-  if (!components || !components[0] || !components[0].startsWith('&')) {
-    return cb(new Error('root in path must be a URL encoded blob id'))
+  var id = (components || []).shift()
+  if (id.startsWith('&')) {
+    // resolve blob
+    fetchBlob(id)
+  } else if (id.startsWith('%')) {
+    // resolve msg id
+    resolveMsgId(id, function (err, blobId) {
+      if (err) return cb(err)
+      fetchBlob(blobId)
+    })
+  } else {
+    return cb(new Error('root in path must be a URL encoded blob id or msg id'))
   }
 
-  var id = components.shift()
+  function fetchBlob (id) {
+    sbot.blobs.size(id, function (err, size) {
+      if (err) return cb(err)
+      if (size == null) {
+        sbot.blobs.want(id, function (err) {
+          if (err) return cb(err)
+          get()
+        })
+      }
+      else get()
 
-  // get root blob
-  sbot.blobs.size(id, function (err, size) {
-    if (err) return cb(err)
-    if (size == null) {
-      sbot.blobs.want(id, function (err) {
-        if (err) return cb(err)
-        get()
+      function get () {
+        pull(sbot.blobs.get(id), pull.collect(function (err, chunks) {
+          if (err) return cb(err)
+          var text = Buffer.concat(chunks).toString()
+          resolveRec(text)
+        }))
+      }
+    })
+  }
+
+  function resolveMsgId (id, cb) {
+    var foundRoot = false
+    pull(
+      sbot.backlinks.read({
+        query: [{$filter: { dest: id } }],
+        index: 'DTA',
+        live: false,
+        reverse: true
+      }),
+      pull.drain(function (msg) {
+        if (!foundRoot) cb(null, msg.value.content.root)
+        foundRoot = true
+      }, function () {
+        if (!foundRoot) cb(new Error('404: no roots published'))
       })
+    )
+  }
+
+  function resolveRec (text) {
+    try {
+      // if this succeeds, it's a directory
+      var json = JSON.parse(text)
+      if (!json.links) throw new Error('show me as a raw file')
+      var link = components.shift()
+
+      // redirect to index.htm[l] if possible
+      if (!link) {
+        link = 'index.html'
+      }
+
+      if (!link) {
+        // if components[1] is empty, return the directory as text
+        return cb(null, text)
+      } else if (!json.links[link]) {
+        // no such link exists
+        return cb(new Error('404'))
+      } else {
+        // recurse
+        resolve(sbot, [json.links[link]].concat(components), cb)
+      }
+    } catch (e) {
+      // if this happens, it's a file blob
+      // if components[1] exists, this is a 404; otherwise we arrived!
+      if (components[0]) {
+        return cb(new Error('404'))
+      } else {
+        cb(null, text)
+      }
     }
-    else get()
-
-    function get () {
-      pull(sbot.blobs.get(id), pull.collect(function (err, chunks) {
-        if (err) return cb(err)
-        var text = Buffer.concat(chunks).toString()
-        try {
-          // if this succeeds, it's a directory
-          var json = JSON.parse(text)
-          if (!json.links) throw new Error('show me as a raw file')
-          var link = components.shift()
-
-          // redirect to index.htm[l] if possible
-          if (!link) {
-            link = json.links['index.html'] || json.links['index.htm']
-          }
-
-          if (!link) {
-            // if components[1] is empty, return the directory as text
-            return cb(null, text)
-          } else if (!json.links[link]) {
-            // no such link exists
-            return cb(new Error('404'))
-          } else {
-            // recurse
-            resolve(sbot, [json.links[link]].concat(components), cb)
-          }
-        } catch (e) {
-          // if this happens, it's a file blob
-          // if components[1] exists, this is a 404; otherwise we arrived!
-          if (components[0]) {
-            return cb(new Error('404'))
-          } else {
-            cb(null, text)
-          }
-        }
-      }))
-    }
-  })
+  }
 }
